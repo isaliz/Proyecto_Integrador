80d555e0fb760093209426291647874bb299f7d4
isaliz <lizarraldeisabel@gmail.com>
2023-08-26 12:58:21 -0500

    Sincronizacion Migue

--- src/IDao.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface IDao<T> {
-    public List<T> buscarTodos();
-    public T agregar(T t);
-    public T  modificar(T t);
-    public void eliminar(Long id);
-}
-
--- src/Login.java
+++
@@ -1,5 +0,0 @@
-public class Login {
-    private String usuario;
-    private String password;
-
-}
