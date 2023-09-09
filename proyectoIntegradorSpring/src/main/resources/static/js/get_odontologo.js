ffe530e0bde87c31dd00c672bc9301c73e01a459
isaliz <lizarraldeisabel@gmail.com>
2023-09-09 12:12:44 -0500

    CRUD realizado con exito para Odontolgo

--- proyectoIntegradorSpring/src/main/resources/static/js/get_odontologo.js
+++ proyectoIntegradorSpring/src/main/resources/static/js/get_odontologo.js
@@ -49,15 +49,5 @@ window.addEventListener('load', function () {
 
 });
 
-function deleteBy(id) {
-    // Elimina el odontólogo de la API.
-    fetch(`/odontologos/${id}`, {
-        method: 'DELETE'
-    });
 
-    // Elimina la fila de la tabla.
-    const table = document.getElementById("odontologosTable");
-    const row = table.querySelector(`#tr_${id}`);
-    row.remove();
-}
 
